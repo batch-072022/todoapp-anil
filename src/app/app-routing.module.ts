3f5354c3b988ede1f44103922de1d244d3371a33
Anil Barla <anil.barla@valuemomentum.com>
2022-08-02 10:45:22 +0530

    todoapp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { AddtaskComponent } from './addtask/addtask.component';
+import { EdittaskComponent } from './edittask/edittask.component';
+import { ListtasksComponent } from './listtasks/listtasks.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'home',component:HomeComponent},
+  {path:'addtask',component:AddtaskComponent},
+  {path:'edittask',component:EdittaskComponent},
+  {path:'listtasks',component:ListtasksComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { LoginUsersComponent } from './login-users/login-users.component';
+import { AddtaskComponent } from './addtask/addtask.component';
+import { EdittaskComponent } from './edittask/edittask.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ListtasksComponent } from './listtasks/listtasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    LoginUsersComponent,
+    AddtaskComponent,
+    EdittaskComponent,
+    HeaderComponent,
+    HomeComponent,
+    ListtasksComponent
   ],
   imports: [
     BrowserModule,
